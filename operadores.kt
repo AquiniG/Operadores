b462001e788b9c96ebad8ba6c2ce42875e8522f7
AquiniG <aquini94@gmail.com>
2026-09-04 18:21:39 -0300

    adiciona operadores logicos

--- operadores.kt
+++ operadores.kt
@@ -5,11 +5,20 @@ fun main() {
     val a = 10
     val b = 5
 
-    println(a == b)
-    println(a > b)
-    println(a < b)
-    println(a != b)
-    println(a >= b)
-    println(a <= b)
+    println(a == b)                            // false: 10 é igual a 5?
+    println(a > b)                             // true: 10 é maior que 5?
+    println(a < b)                             // false: 10 é menor que 5?
+    println(a != b)                            // true: 10 é diferente de 5?
+    println(a >= b)                            // true: 10 é maior ou igual a 5?
+    println(a <= b)                            // false: 10 é menor ou igual a 5?
+    println()
+
+    val temIngresso = true
+    val temIdadeMinima = false
+
+    println(temIngresso && temIdadeMinima)     // false: "E": as DUAS precisam ser true
+    println(temIngresso || temIdadeMinima)     // true: "OU": basta UMA ser true
+    println(!temIngresso)                      // false: "NÃO": inverte o valor
+
 
 }
